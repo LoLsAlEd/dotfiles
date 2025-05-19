f1cc50c5b5e9dadbf887b2d458e25fff0612bdee
lolsaled <13088744+LoLsAlEd@users.noreply.github.com>
2025-05-19 22:08:30 +0930

    Add Ansible Setup Script

---
+++ run_once_install_brew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+install_on_mac() {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+OS="$(uname -s)"
+case "${OS}" in
+    Linux*)
+        echo "Linux detected skipping brew install"
+        exit 0
+        ;;
+    Darwin*)
+        install_on_mac
+        ;;
+    *)
+        echo "Unsupported operating system: ${OS}"
+        exit 1
+        ;;
+esac
+
+echo "Brew installation complete."
