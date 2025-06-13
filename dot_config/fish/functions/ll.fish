92b670372551577c2274d0e843c1a2f4907dd8f5
lolsaled <13088744+LoLsAlEd@users.noreply.github.com>
2025-06-13 21:32:40 +0930

    Add ll function

---
+++ dot_config/fish/functions/ll.fish
@@ -0,0 +1,4 @@
+function ll --wraps='eza -la --icons --octal-permissions --group-directories-first --icons' --description 'alias ll eza -la --icons --octal-permissions --group-directories-first --icons'
+  eza -la --icons --octal-permissions --group-directories-first --icons $argv
+        
+end
